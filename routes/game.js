882bedb9225c3dcc65ae9bbffb258a8c7dc57dba
sauravmh <sauravhiremath@gmail.com>
2020-01-03 14:33:48 +0530

    feat: [WIP] Socket functions

--- routes/game.js
+++ routes/game.js
@@ -1,13 +1,125 @@
-const express = require("express");
-const server = require("http").createServer(express());
-const io = require("socket.io")(server);
+let io;
+let gameSocket;
+const rooms = {};
+let words = [];
 
-const logger = require("../tools/loggers");
+function randomProperty(object) {
+	const keys = Object.keys(object);
+	return keys[Math.floor(keys.length * Math.random())];
+}
 
-const router = express.Router();
+const playerJoinRoom = (data) => {
+	const { gameID } = data;
+	data.mySocketID = this.id;
+	this.join(gameID.toString());
+	this.in(gameID).emit("playerJoinedRoom", data);
+};
 
-router.get("/", (req, res) => {
-	res.render("index");
-});
+const startNewMatch = (data) => {
+	const { gameID } = data;
+	try {
+		const roomSize = io.sockets.adapter.rooms[gameID.toString()].length;
+		// Will throw error on roomSize=0, as rooom isn't yet created
+		if (roomSize >= 2) {
+			rooms[data] = "playing";
+			this.in(gameID).emit("newMatchStarted", data);
+		}
+	} catch (err) {
+		const roomSize = 0;
+	}
+};
 
-module.exports = router;
+function createNewRound(data) {
+	const gameID = Math.random() * 10 || 0;
+	data.gameID = gameID;
+	data.mySocketID = this.id;
+	this.join(gameID.toString());
+	this.emit("newRoundStarted", data);
+}
+
+function updatePlayerPlayersServer(data) {
+	io.sockets.in(data[0].gameID).emit("updatePlayerPlayers", data);
+}
+function chatMessage(chatData) {
+	io.sockets.in(chatData.gameID).emit("newChatMessage", chatData);
+}
+function startGame(data) {
+	// logger.log(data);
+	io.sockets.in(data).emit("prepareStartGame", {
+		word: words.arr[Math.floor(words.arr.length * Math.random())],
+	});
+	rooms[data] = "playing";
+}
+function mousemove(data, drawerWindowSize) {
+	this.broadcast.to(data.gameID).emit("isMoving", data, drawerWindowSize);
+}
+function gameEnd(data) {
+	// {winner_name}
+	// logger.info(data);
+	rooms[data.gameID] = "waiting";
+	io.sockets.in(data.gameID).emit("gameEnded", data.name);
+}
+function updateServerChatHistory(data, chatHistory) {
+	io.sockets.in(data.gameID).emit("saveChatHistory", chatHistory);
+}
+function startDrawingTimer(data, turnLength, start) {
+	io.sockets.in(data).emit("startTimer", turnLength, start);
+}
+function broadcastTimer(data, secs) {
+	io.sockets.in(data).emit("updateTimer", secs);
+}
+function givePoints(data) {
+	// logger.log(data.gameID);
+	io.sockets.in(data.gameID).emit("updateUserPoints", data);
+}
+function broadcastNewColor(gameID, color) {
+	io.sockets.in(gameID).emit("receiveNewColor", color);
+}
+function broadcastNewThickness(gameID, drawThickness) {
+	io.sockets.in(gameID).emit("receiveNewDrawThickness", drawThickness);
+}
+function restartDrawPath(data) {
+	io.sockets.in(data).emit("restartPath");
+}
+function updateTurn(data) {
+	this.broadcast.to(data.gameID).emit("updatePlayerTurn", data);
+}
+function clearCurrentCanvas(data) {
+	io.sockets.in(data).emit("clearDrawingCanvas");
+}
+function playerLeft(data, userInformation) {
+	io.sockets.in(data).emit("userHasLeft", userInformation);
+}
+function endGameLobby(data) {
+	io.sockets.in(data).emit("gameEndedLobby");
+}
+
+const initGame = (sio, socket, wordList) => {
+	// client --> server --> clients (chat message from client)
+	io = sio;
+	gameSocket = socket;
+	words = wordList;
+	gameSocket.emit("connected", { message: "You are connected!" });
+	// Host Events
+	gameSocket.on("createNewMatch", startNewGame);
+	gameSocket.on("createNewRound", createNewRound);
+	gameSocket.on("playerJoinGame", playerJoinGame);
+	gameSocket.on("updatePlayerPlayersServer", updatePlayerPlayersServer);
+	gameSocket.on("chatMessage", chatMessage);
+	gameSocket.on("startGame", startGame);
+	gameSocket.on("mousemove", mousemove);
+	gameSocket.on("gameEnd", gameEnd);
+	gameSocket.on("updateServerChatHistory", updateServerChatHistory);
+	gameSocket.on("startDrawingTimer", startDrawingTimer);
+	gameSocket.on("givePoints", givePoints);
+	gameSocket.on("broadcastTimer", broadcastTimer);
+	gameSocket.on("broadcastNewColor", broadcastNewColor);
+	gameSocket.on("broadcastNewThickness", broadcastNewThickness);
+	gameSocket.on("restartDrawPath", restartDrawPath);
+	gameSocket.on("updateTurn", updateTurn);
+	gameSocket.on("clearCurrentCanvas", clearCurrentCanvas);
+	gameSocket.on("playerLeft", playerLeft);
+	gameSocket.on("endGameLobby", endGameLobby);
+};
+
+export default initGame;
--- server.js
+++ server.js
@@ -4,13 +4,13 @@ require("./models/db");
 const express = require("express");
 const path = require("path");
 const bodyparser = require("body-parser");
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 const { logger } = require("./tools/loggers");
 const gameRouter = require("./routes/game");
 
-const app = require("express")();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
 const port = process.env.PORT || 3000;
 
 if (!process.env.JWT_SECRET) {
@@ -28,11 +28,15 @@ app.use(
 	bodyparser.json(),
 );
 
-//Socket stuff
-io.on('connection', client => {
-	logger.info('Client Connected');
-	client.on('event', data => { logger.info('Some event happend'); });
-	client.on('disconnect', () => { logger.info('Client disconnected'); });
+// Socket stuff
+io.on("connection", (client) => {
+	logger.info("Client Connected");
+
+	require("pathToSocketRoutesFile1")(client);
+	require("pathToSocketRoutesFile2")(client);
+	require("pathToSocketRoutesFileN")(client);
+
+	return io;
 });
 
 server.listen(port, () => {
