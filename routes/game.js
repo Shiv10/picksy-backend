0764c4719a0be32798defb8f818ec57463ce17cd
sauravmh <sauravhiremath@gmail.com>
2020-02-09 18:07:15 +0530

    update: Code refactoring

--- routes/game.js
+++ routes/game.js
@@ -1,21 +1,34 @@
+/* eslint-disable no-use-before-define */
 const socketio = require("socket.io");
+
 const { logger } = require("../tools/loggers");
 
 const users = {};
 const names = {};
-let keys = []
-const words = ["pen", "paper", "glasses", "bottle", "keyboard", "sun", "hills", "glue", "keys", "box"]
+let keys = [];
+const words = [
+	"pen",
+	"paper",
+	"glasses",
+	"bottle",
+	"keyboard",
+	"sun",
+	"hills",
+	"glue",
+	"keys",
+	"box",
+];
 let userCount = 0;
 
 const room = {
 	roundNumber: 0,
 	turn: {
 		start: false,
-		timeStart: 0.0
+		timeStart: 0.0,
 	},
 	currentWord: "",
-	currentDrawer: ""
-}
+	currentDrawer: "",
+};
 let turn = 0;
 let turnOn = false;
 
@@ -23,100 +36,54 @@ module.exports.listen = (app) => {
 	const io = socketio.listen(app);
 
 	io.on("connection", (socket) => {
-		// logger.info("Client connected");
-		// gameStart (initial)
-		// RoundStart (drawer select)
-		// wordSelection (send timestamp, (give 5 secs), give word options, select the word)
-
-		// - broadcast Timer (send timestamp)
-		// - canvas Draw (send matrix to all clients)
-
-		// playerJoin (create stack, add draw points to it, emit the stack, contents)
-		// chat MessageChannel (correct, wrong, close guesses all handled here)
-		// display score (score at end of each round)
-
 		socket.on("new user", (name) => {
 			logger.info("user connected");
 			names[name] = socket;
 			users[socket.id] = name;
-			//logger.info(users);
-			//logger.info(names);
+			// logger.info(users);
+			// logger.info(names);
 			keys = Object.keys(names);
-			userCount++;
-
+			userCount += 1;
 		});
 
 		keys = Object.keys(names);
-		if ((keys.length + 1) == 2) {
+		if (keys.length + 1 === 2) {
 			io.emit("start-game");
 			room.turn.start = true;
-			changeTurn();
+			changeTurn(io);
 		}
 
-		function changeTurn() {
-			if (!room.turn.start) return;
-
-			room.currentDrawer = keys[turn];
-			n1 = Math.floor(Math.random() * 10);
-			n2 = Math.floor(Math.random() * 10);
-			n3 = Math.floor(Math.random() * 10);
-			names[room.currentDrawer].emit("word-selection", { w1: words[n1], w2: words[n2], w3: words[n3] });
-			room.turn.start = false;
-		};
-
-
-		if ((keys.length + 1) > 2) {
+		if (keys.length + 1 > 2) {
 			socket.emit("start-game");
 			if (turnOn) {
-				socket.emit("word-selected", { name: room.currentDrawer, time: room.turn.timeStart });
+				socket.emit("word-selected", {
+					name: room.currentDrawer,
+					time: room.turn.timeStart,
+				});
 			}
-
 		}
 
 		socket.on("word-selected", (data) => {
 			const timeStamp = new Date();
 			logger.info(data.word);
 			room.currentWord = data.word;
-			ct = Math.floor(timeStamp.getTime() / 1000);
+			const ct = Math.floor(timeStamp.getTime() / 1000);
 			room.turn.timeStart = ct;
 			io.emit("word-selected", { name: room.currentDrawer, time: ct });
 			turnOn = true;
-			setTimeout(turnChange, 82000);
-			function turnChange() {
-				logger.info("turn over!");
-				turn++;
-				if (turn == userCount) {
-					roundChange();
-					turnOn = false;
-				}
-				//names[room.currentDrawer].emit("round-end");
-				else {
-					names[room.currentDrawer].emit("turn-end");
-					io.emit("canvas-cleared");
-					room.turn.start = true;
-					changeTurn();
-				}
-			};
+			setTimeout(turnChange, 82000, io);
 		});
 
-		function roundChange() {
-			names[room.currentDrawer].emit("turn-end");
-			io.emit("round-end");
-			room.roundNumber++;
-		}
-
 		socket.on("message", (data) => {
-			if (data.text == room.currentWord) {
-				logger.info(users[socket.id] + " guessed!");
+			if (data.text === room.currentWord) {
+				logger.info(`${users[socket.id]} guessed!`);
 				io.emit("word-guessed", { name: users[socket.id] });
-			}
-			else {
+			} else {
 				socket.broadcast.emit("message", {
 					name: users[socket.id],
-					text: data.text
+					text: data.text,
 				});
 			}
-
 		});
 
 		socket.on("draw", (data) => {
@@ -129,17 +96,50 @@ module.exports.listen = (app) => {
 
 		socket.on("canvas-cleared", () => {
 			socket.broadcast.emit("canvas-cleared");
-		})
+		});
 
 		socket.on("disconnect", () => {
 			logger.info("disconnected");
 			delete names[users[socket.id]];
-			//logger.info(names);
+			// logger.info(names);
 			delete users[socket.id];
 		});
-
-
 	});
 
 	return io;
 };
+
+function changeTurn() {
+	if (!room.turn.start) return;
+
+	room.currentDrawer = Object.keys(names)[turn];
+	const n1 = Math.floor(Math.random() * 10);
+	const n2 = Math.floor(Math.random() * 10);
+	const n3 = Math.floor(Math.random() * 10);
+	names[room.currentDrawer].emit("word-selection", {
+		w1: words[n1],
+		w2: words[n2],
+		w3: words[n3],
+	});
+	room.turn.start = false;
+}
+
+function roundChange(io) {
+	names[room.currentDrawer].emit("turn-end");
+	io.emit("round-end");
+	room.roundNumber += 1;
+}
+
+function turnChange(io) {
+	logger.info("turn over!");
+	turn += 1;
+	if (turn === userCount) {
+		roundChange(io);
+		turnOn = false;
+	} else {
+		names[room.currentDrawer].emit("turn-end");
+		io.emit("canvas-cleared");
+		room.turn.start = true;
+		changeTurn();
+	}
+}
