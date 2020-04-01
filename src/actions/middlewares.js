565c0a52ab79d85c256b29f391439899ced1da4a
Saurav M. H <sauravhiremath@gmail.com>
2020-04-01 23:25:13 +0530

    feat: PORT used from env

--- src/actions/middlewares.js
+++ src/actions/middlewares.js
@@ -1,7 +1,9 @@
 import User from "../models";
 import constants from "../tools/constants";
+import { logger } from "../tools/loggers";
 
-const whitelist = process.env.NODE_ENV === "production" ? constants.url : constants.urlDev;
+const whitelist = process.env.NODE_ENV === "production" ? Object.values(constants.url) : Object.values(constants.urlDev);
+logger.info((whitelist));
 
 export default {
 	origin(origin, callback) {
--- src/tools/constants.js
+++ src/tools/constants.js
@@ -1,21 +1,24 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export default {
 	url: ["https://piksy.sauravmh.me", "https://pik-soc.sauravmh.me"],
-	urlDev: ["localhost:3001", "localhost:3002"],
+	urlDev: [`http://localhost:${process.env.PORT}`, `http://localhost:${process.env.PORT}`],
 	roundNum: 3,
 	matchLen: 10000,
 	wordSelOptions: 3,
 	timeOfRound: 80000,
 	playerPointFactor: 10,
 	drawerPointFactor: 8,
-	corsOptions: {
-		handlePreflightRequest: (req, res) => {
-			const headers = {
-				"Access-Control-Allow-Headers": "Content-Type, Authorization",
-				"Access-Control-Allow-Origin": process.env.NODE_ENV === "production" ? "https://piksy.sauravmh.me" : "http://localhost:3001", // or the specific origin you want to give access to,
-				"Access-Control-Allow-Credentials": true,
-			};
-			res.writeHead(200, headers);
-			res.end();
-		},
+	corsOptions: (req, res) => {
+		const headers = {
+			"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			"Access-Control-Allow-Origin":
+				process.env.NODE_ENV === "production" ? "https://piksy.sauravmh.me" : `http://localhost:${process.env.PORT}`, // or the specific origin you want to give access to,
+			"Access-Control-Allow-Credentials": true,
+		};
+		res.writeHead(200, headers);
+		res.end();
 	},
 };
