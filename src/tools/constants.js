3ddac6a2fc1ba26114cda0f66f1506abaf7acf63
Saurav M. H <sauravhiremath@gmail.com>
2020-04-01 22:34:07 +0530

    feat: Added option for deployment

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -1,4 +1,6 @@
 export default {
+	url: ["https://piksy.sauravmh.me", "https://pik-soc.sauravmh.me"],
+	urlDev: ["localhost:3001", "localhost:3002"],
 	roundNum: 3,
 	matchLen: 10000,
 	wordSelOptions: 3,
@@ -9,7 +11,7 @@ export default {
 		handlePreflightRequest: (req, res) => {
 			const headers = {
 				"Access-Control-Allow-Headers": "Content-Type, Authorization",
-				"Access-Control-Allow-Origin": "http://localhost:3001", // or the specific origin you want to give access to,
+				"Access-Control-Allow-Origin": process.env.NODE_ENV === "production" ? "https://piksy.sauravmh.me" : "http://localhost:3001", // or the specific origin you want to give access to,
 				"Access-Control-Allow-Credentials": true,
 			};
 			res.writeHead(200, headers);
